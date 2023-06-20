82744ca68db925a348ed425bca01898ed5a8eace
Yonaseyob <yonaseyobberhane@gmail.com>
2023-06-20 16:28:14 +0000

    Add local host

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestary.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestary.herokuapp.com', 'localhost', '8000-yonaseyob-djangoblog-mjj7h5mtkqq.ws-eu100.gitpod.io']
 
 
 # Application definition
