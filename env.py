f97c25f59e89129540c773a9e9d7a6cacd53372a
Yonaseyob <yonaseyobberhane@gmail.com>
2023-06-20 15:35:33 +0000

    Add cloudinary inviron

--- env.py
+++ env.py
@@ -2,4 +2,5 @@ import os
 
 os.environ["DATABASE_URL"]="postgres://csnasgut:ozEd081j2JQz5dpQizba_hqEWPj-Dfkz@trumpet.db.elephantsql.com/csnasgut"
 os.environ["SECRET_KEY"]="my_first_key@key"
+os.environ["CLOUDINARY_URL"] = "cloudinary://638216569397896:c3JZQeXjR93lEAEyrM0X0-Z1LWs@dhjgfeflg"
 
